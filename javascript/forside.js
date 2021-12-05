963fd9f1c0e22c6ae813bb0f65a4fbeebaa496bc
FarissCreative <83574304+FarissCreative@users.noreply.github.com>
2021-12-05 23:20:33 +0100

    made header responsive and functionality to the burger-bar menu
    added javascript to the burger menu and fixed a responsive design to the header/navigation

---
+++ javascript/forside.js
@@ -0,0 +1,48 @@
+//Nadin fariss
+
+//navigation bar 
+//console.log("works");
+
+let hamburger, menu, menuItems, closeMenu, closeIcon;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    hamburger = document.querySelector("#hamburger");
+    menu = document.querySelector(".nav__list-container");
+    menuItems = document.querySelectorAll(".nav__list-item");
+    closeMenu = document.querySelector(".nav__list-item--active");
+    closeIcon = document.querySelector(".nav__icon2");
+
+    hamburger.addEventListener("click", toggleMenu);
+    closeIcon.addEventListener("click", toggleMenu);
+
+    menu.style.visibility = "hidden";
+})
+
+function toggleMenu() {
+    //hamburger;
+    //console.log("function works")
+
+    if (menu.style.visibility === "hidden") {
+        menu.classList.add("showMenu");
+        //menu.classList.remove("closeMenu");
+        menu.style.visibility = "visible";
+        //menuItems.style.visibility = "visible";
+        hamburger.style.display = "none";
+        closeIcon.style.display = "initial";
+        //insert animation
+    }
+    else {
+        menu.style.visibility = "hidden";
+        menu.classList.remove("showMenu");
+        //menu.classList.add("closeMenu");
+        closeIcon.style.display = "none";
+        hamburger.style.display = "initial";
+        //menuItems.style.visibility = "hidden";
+    }
+}
+
+
+
+
+
+//manglende pensum
