f3c36c9a0d65297ca079c61b31719f9c16bbe6fa
jonnamh <jonna.mh@hotmail.com>
2021-12-09 13:17:00 +0100

    Lavet popup til nyhedsbrev (js)

---
+++ javascript/nyhedsbrev-jonna.js
@@ -0,0 +1,22 @@
+//jonna
+document.getElementById("cta1").addEventListener("click", function() {
+  document.querySelector(".nyhedspopup").style.display = "flex";
+})
+
+document.getElementById("nyhedspopup-kryds").addEventListener("click", function() {
+  document.querySelector(".nyhedspopup").style.display = "none";
+})
+
+//Pensum
+var minGruppe=['Xenia', 'Nadin', 'Nicoline', 'Jonna'];
+
+  if (minGruppe.length = 4) {
+    console.log("Gruppen består af " + minGruppe.length + " medlemmer");
+
+  } else {
+    console.log("For få medlemmer, skaf en ekstra!");
+}
+
+for (var i = 0; i < minGruppe.length; i++) {
+  minGruppe[i]
+}
