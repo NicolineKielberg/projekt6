468139c57d5b628f539c814fbf325e3477cb489b
FarissCreative <83574304+FarissCreative@users.noreply.github.com>
2021-12-13 15:29:55 +0100

    restoring changes
    har samlet xenia og nicolines code i samme js file, da der er opstået komplikationer, hvor den hurtige løsningen har været at samle deres logik, hvor der står hvem der står for hvilket afsnit.

--- javascript/xeniaognicoline.js
+++ javascript/xeniaognicoline.js
@@ -1,4 +1,33 @@
+//xenia 
+document.getElementById("knap").addEventListener("click", function () {
+  document.querySelector(".container10").style.display = "flex";
+})
 
+//pensum
+let juleønsker = ["mikroovn", "apple watch", "tinderbox", "føntørrer", "harman kardon højtaler"];
+
+
+for (var i = 0; i < juleønsker.length; i++) {
+  console.log(juleønsker[i]);
+}
+
+let a = 5;
+let b = 10;
+
+if (a > b) {
+  console.log('xenia er sjov');
+
+} else if (a < b) {
+  console.log('xenia er ekstremt sjov');
+}
+else {
+  console.log('xenia er ikke sjov');
+}
+
+
+
+
+// Nicoline
 document.getElementById("grid__btn2").addEventListener("click", function () {
   document.querySelector(".tilmeldpopup").style.display = "flex";
 })
@@ -8,7 +37,6 @@ document.getElementById("tilmeldpopup-kryds").addEventListener("click", function
 })
 
 
-
 var juleliste = ['Ganni hue', 'New balance sko', 'Ganni hoodie', 'Kay Bojesen'];
 
 
@@ -16,13 +44,13 @@ for (var i = 0; i < juleliste.length; i++) {
   console.log(juleliste[i]);
 }
 
-var a = 20;
-var b = 20;
+var c = 20;
+var d = 20;
 
-if (a < b) {
+if (c < d) {
   console.log('jeg får de gaver jeg ønsker mig');
 }
-else if (a > b) {
+else if (c > d) {
   console.log('Jeg får ikke de gaver jeg øsnker mig');
 }
 else {
