55a0d662f0d106dab8e7d4c8ecc64a2577c7f7be
FarissCreative <83574304+FarissCreative@users.noreply.github.com>
2021-12-13 14:07:09 +0100

    commit fejl restored code

--- javascript/tilmeld-popup-nicoline.js
+++ javascript/tilmeld-popup-nicoline.js
@@ -1,7 +1,30 @@
-document.getElementById("grid__btn2").addEventListener("click", function() {
+
+document.getElementById("grid__btn2").addEventListener("click", function () {
   document.querySelector(".tilmeldpopup").style.display = "flex";
 })
 
-document.getElementById("tilmeldpopup-kryds").addEventListener("click", function() {
+document.getElementById("tilmeldpopup-kryds").addEventListener("click", function () {
   document.querySelector(".tilmeldpopup").style.display = "none";
 })
+
+
+
+var juleliste = ['Ganni hue', 'New balance sko', 'Ganni hoodie', 'Kay Bojesen'];
+
+
+for (var i = 0; i < juleliste.length; i++) {
+  console.log(juleliste[i]);
+}
+
+var a = 20;
+var b = 20;
+
+if (a < b) {
+  console.log('jeg får de gaver jeg ønsker mig');
+}
+else if (a > b) {
+  console.log('Jeg får ikke de gaver jeg øsnker mig');
+}
+else {
+  console.log('jeg får i det mindste en eller anden julegave');
+}
